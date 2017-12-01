3e51bee6da8824ddffebe5f45c8df4a66df3bbeb
tatwd <leoking9641@hotmail.com>
2017-12-01 11:45:38 +0800

    Add `Dal` class lib by _king

---
+++ Dal/Dao/BaseDao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal.Dao
+{
+    public class BaseDao
+    {
+        // TODO: 定义 CRUD 操作
+    }
+}
