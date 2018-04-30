2405c745aba9faa64073052ce3beba070f02ce09
--global <leoking9641@hotmail.com>
2018-04-30 21:42:20 +0800

    rebuild project structure

--- bermuda-client/src/router/index.js
+++ bermuda-client/src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 
 // import Example from '@/components/Example'
 import HomeIndex from '@/components/Home/Index'
+import Index from '@/views/index'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'HomeIndex',
       component: HomeIndex
+    },
+    {
+      path: '/index',
+      name: 'Index',
+      component: Index
     }
   ],
   mode: 'hash'
---
+++ bermuda-client/src/views/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <HomeIndex/>
+  </div>
+</template>
+
+<script>
+import HomeIndex from '@/components/Home/Index'
+
+export default {
+  data () {
+    return {
+      example: 'views/index'
+    }
+  },
+  components: {
+    HomeIndex
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
