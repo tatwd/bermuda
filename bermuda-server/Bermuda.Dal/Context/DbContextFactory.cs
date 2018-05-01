4d14559207649d689dcd980a2b000168e66aba56
--global <leoking9641@hotmail.com>
2018-05-01 14:26:12 +0800

    add db context factory

---
+++ bermuda-server/Bermuda.Dal/Context/DbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bermuda.Dal.Context
+{
+    using System.Data.Entity;
+
+    class DbContextFactory
+    {
+        /// <summary>
+        /// 创建EF上下文对象，已存在就直接取，不存在就创建，保证线程内是唯一
+        /// </summary>
+        public static DbContext GetDbContext()
+        {
+            // TODO
+            return null;
+        }
+    }
+}
