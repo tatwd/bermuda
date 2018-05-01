f9033a2fb7a3b73fcbdc3bd8c4574d9ee4c42bd6
--global <leoking9641@hotmail.com>
2018-05-01 14:48:08 +0800

    fix

--- bermuda-server/Bermuda.Dal/Context/DbContextFactory.cs
+++ bermuda-server/Bermuda.Dal/Context/DbContextFactory.cs
@@ -1,22 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Bermuda.Dal.Context
+﻿namespace Bermuda.Dal.Context
 {
     using System.Data.Entity;
+    using System.Runtime.Remoting.Messaging;
+    using Model;
 
     class DbContextFactory
     {
+        const string CONTEXT_KEY = "BmdDbContext";
+
         /// <summary>
-        /// 创建EF上下文对象，已存在就直接取，不存在就创建，保证线程内是唯一
+        /// 创建 EF 上下文对象，已存在就直接取，不存在就创建，保证线程内是唯一
         /// </summary>
         public static DbContext GetDbContext()
         {
-            // TODO
-            return null;
+            DbContext context = CallContext.GetData(CONTEXT_KEY) as DbContext;
+
+            if(context == null)
+            {
+                context = new BmdDbEntities();
+                CallContext.SetData(CONTEXT_KEY, context);
+            }
+
+            return context;
         }
     }
 }
