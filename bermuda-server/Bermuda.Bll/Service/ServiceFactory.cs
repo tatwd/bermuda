a1c37d677e3fbe6f130241592dcebc7daed9f11a
--global <leoking9641@hotmail.com>
2018-05-02 11:56:15 +0800

    add service factory

---
+++ bermuda-server/Bermuda.Bll/Service/ServiceFactory.cs
@@ -0,0 +1,11 @@
+﻿namespace Bermuda.Bll.Service
+{
+    public class ServiceFactory
+    {
+        public static IBaseService<T> GetService<T>()
+            where T : class, new()
+        {
+            return null;
+        }
+    }
+}
