01a9c86b09e6263e7ce3a862e5dd96357aa126e4
--global <leoking9641@hotmail.com>
2018-05-15 18:21:28 +0800

    add account layout

---
+++ bermuda-client/src/layouts/account.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="layout--account">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'account',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
