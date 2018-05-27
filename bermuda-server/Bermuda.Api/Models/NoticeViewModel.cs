08536c1f0f608e7f0d8400b557f0ca7f53c57f07
--global <leoking9641@hotmail.com>
2018-05-27 21:57:22 +0800

    todo: fix search api

--- bermuda-server/Bermuda.Api/Models/NoticeViewModel.cs
+++ bermuda-server/Bermuda.Api/Models/NoticeViewModel.cs
@@ -25,4 +25,24 @@ namespace Bermuda.Api.Models
         public Int64 trace_count { get; set; }
         public byte is_solved { get; set; }
     }
+
+    public class NoticeSearchModel
+    {
+        public Int64 id { get; set; }
+        public Int64 user_id { get; set; }
+        public string user_name { get; set; }
+        public Int64 specie_id { get; set; }
+        public string type { get; set; }
+        public string title { get; set; }
+        public string place { get; set; }
+        public string full_place { get; set; }
+        public string event_time_desc { get; set; }
+        public string img_url { get; set; }
+        public string contact_way { get; set; }
+        public string detail { get; set; }
+        public DateTime created_at { get; set; }
+        public Int64 cmnt_count { get; set; }
+        public Int64 trace_count { get; set; }
+        public byte is_solved { get; set; }
+    }
 }
